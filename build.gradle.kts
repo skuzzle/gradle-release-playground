c58a5c1b139a83cab9c307705ce9c526986a3e20
Simon Taddiken <staddiken@gradle.com>
2023-09-08 17:09:42 +0200

    update plöugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import pl.allegro.tech.build.axion.release.domain.preRelease
 
 plugins {
-    id("pl.allegro.tech.build.axion-release") version "1.15.1"
+    id("pl.allegro.tech.build.axion-release") version "1.15.4"
     id("com.github.breadmoirai.github-release") version "2.4.1"
 }
 
