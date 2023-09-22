306464e541f62f7ce20267feacd74c13a5dd1630
Simon Taddiken <staddiken@gradle.com>
2023-09-22 15:39:05 +0200

    ordering

--- build-logic/src/main/kotlin/ReleaseHookTask.kt
+++ build-logic/src/main/kotlin/ReleaseHookTask.kt
@@ -1,11 +1,13 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.work.DisableCachingByDefault
 
 @DisableCachingByDefault(because = "Not worth caching")
 abstract class ReleaseHookTask : DefaultTask() {
 
+    @get:Input
     abstract val alsoDependsOn: Property<Task>
 
     fun releaseDependsOn(task: Task) {
