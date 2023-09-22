9cf7da70bee4d736267ca1f5e0e38eacf96b88db
Simon Taddiken <staddiken@gradle.com>
2023-09-22 14:08:34 +0200

    ordering

--- build-logic/src/main/kotlin/release-conventions.gradle.kts
+++ build-logic/src/main/kotlin/release-conventions.gradle.kts
@@ -88,6 +88,7 @@ val release by tasks.creating(DefaultTask::class.java) {
 rootProject.subprojects {
     val beforeReleaseHook by this.tasks.creating(DefaultTask::class.java) {
         mustRunAfter(checkCleanWorkingCopy)
+        outputs.upToDateWhen { false }
     }
     release.dependsOn(this.tasks.named("beforeReleaseHook"))
 }
